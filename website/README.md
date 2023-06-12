16a4c726a56acd03c0b87a086abd41485b050af2
Yogesh <135326860+yogeshprettybaby@users.noreply.github.com>
2023-06-12 13:20:08 +0530

    Add files via upload

---
+++ website/README.md
@@ -0,0 +1,2 @@
+# This is My First WebSite, ❤️💚💙
+https://yogeshbabyop.github.io/website/
---
+++ website/index.js
@@ -0,0 +1,31 @@
+document.getElementById("m4button").onclick = function(){
+    window.alert("This is M416...❤️")
+}      
+
+document.getElementById("akmbutton").onclick = function(){
+    window.alert("This is AKM...❤️")
+}      
+
+document.getElementById("scarelbutton").onclick = function(){
+    window.alert("This is SCAR_L...❤️")
+}      
+
+document.getElementById("qbzbutton").onclick = function(){
+    window.alert("This is QBZ...❤️")
+}      
+
+document.getElementById("m762button").onclick = function(){
+    window.alert("This is BERYL...❤️")
+}      
+
+document.getElementById("augbutton").onclick = function(){
+    window.alert("This is AUG...❤️")
+}      
+
+document.getElementById("grozabutton").onclick = function(){
+    window.alert("This is GROZA...❤️")
+}      
+
+document.getElementById("m16a4button").onclick = function(){
+    window.alert("This is M16A4...❤️")
+}      
